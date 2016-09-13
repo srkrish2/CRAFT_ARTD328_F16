8adf39973bf57bbcddcc97cad6e4307517852280
snehakrishna <sneha.krishna@gmail.com>
2016-09-13 01:57:16 -0500

    aesthetics

--- designer/save_rating.php
+++ designer/save_rating.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<script type='text/javascript'>alert('".$_POST."');</script>";
+echo "<script type='text/javascript'>alert('". print_r($_POST)."');</script>";
 session_start();
 if(!$_SESSION['designer_id']){header('Location: ../index.php');  die();}
 if(!$_GET['design_id']){header('Location: ../index.php');  die();}
