38bb02af2be07d916035a79bdad707e5c100b7b3
snehakrishna <sneha.krishna@gmail.com>
2016-09-12 19:17:45 -0500

    aesthetics

--- designer/design_form.php
+++ designer/design_form.php
@@ -30,7 +30,27 @@
 
   <body>
     <!-- Nav Bar-->
-     <?php include('../webpage-utility/ele_nav.php');?>
+     <nav class="navbar navbar-fixed-top navbar-inverse" style="background:#002058">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" style="color:#E87722" href="../index.php">CRAFT</a>
+            </div>
+            <button type="button" class="btn btn-default navbar-btn navbar-right" id="logout">Logout</button>
+            
+        </div>
+      
+	  </nav>
+	<script>
+	  $(document).ready(function(){
+	    $('#logout').click(function(){
+	        alert("logging out");
+	        $.post('logout.php', function (response) {
+	            window.location.href='../index.php';
+	        });
+	    });
+
+	});
+	</script>
     <!-- End Nav Bar-->
 
     <div class="main-section">
