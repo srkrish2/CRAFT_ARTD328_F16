7fe7ce47eff028f1df4c85c45a4dadae2ccc9e40
snehakrishna <sneha.krishna@gmail.com>
2016-09-18 22:13:38 -0500

    aesthetics

--- designer/save_design.php
+++ designer/save_design.php
@@ -57,7 +57,7 @@ $isOkay=true;
 			$ext = end((explode(".", $name)));
 			$target_file="t".$team_id."_s".$_stage."_d".$design_id.".".$imageFileType;
 			$newFilePath= $target_dir.$target_file;
-			echo '<script type="text/javascript">alert("' . $_FILES["fileToUpload"]["tmp_name"] . '"); </script>';
+			echo '<script type="text/javascript">alert("' . $_upload_file . '"); </script>';
 
 			if(file_exists($newFilePath)) {
 			   // echo "delete first";
