2d82035c0d030639ca01f8275bef0c7b3f54fbae
snehakrishna <sneha.krishna@gmail.com>
2016-11-13 00:28:04 -0600

    server change

--- webpage-utility/db_config.php
+++ webpage-utility/db_config.php
@@ -1,8 +1,8 @@
 <?php
 
-   $DB_HOST = "crowdsight.web.engr.illinois.edu";
-   $DB = "crowdsig_artd328_f16";
-   $SQL_ACC = "crowdsig_1";
+   $DB_HOST = "srkrish2.web.engr.illinois.edu";
+   $DB = "srkrish2_artd328_f16";
+   $SQL_ACC = "srkrish2_1";
    $SQL_PWD = "bpteam!";
 
 ?>
